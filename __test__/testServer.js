38798fb88f460ddbaf990578f725536c6a7c16c1
Divija Joshi <116181095+DivijaJoshi@users.noreply.github.com>
2026-04-05 00:06:18 +0530

    day 5 task done

--- __test__/testServer.js
+++ __test__/testServer.js
@@ -7,7 +7,6 @@ const userRoutes=require('../routes/userRoutes');
 const errorHandler=require('../middlewares/errorHandler')
 const cookieParser = require('cookie-parser');
 
-
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth',authRoutes)
