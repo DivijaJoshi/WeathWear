1eb1793e0f03f175940abfa04ddaa2e4ff2af3e6
Divija Joshi <116181095+DivijaJoshi@users.noreply.github.com>
2026-04-07 20:03:55 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const { signup, login, refreshToken } = require('../controllers/authController');
 const { signupSchema, loginSchema } = require('../schemas/authSchemas');
 const validate = require('../middlewares/validate');
-const authLimiter = require('../middlewares/authLimiter');
 
-router.post('/signup', authLimiter, validate(signupSchema), signup);
-router.post('/login', authLimiter, validate(loginSchema), login);
+router.post('/signup', validate(signupSchema), signup);
+router.post('/login', validate(loginSchema), login);
 router.post('/refresh', refreshToken);
 
 
